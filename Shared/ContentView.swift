fabb386d39e99b6aa95160ee0fc0562510cd16ef
Ben Musoke-Lubega <bprinsta@gmail.com>
2022-07-23 11:00:58 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Benjamin Musoke-Lubega on 7/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/FilterioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterioApp.swift
+//  Shared
+//
+//  Created by Benjamin Musoke-Lubega on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct FilterioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
